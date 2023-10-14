098e5ffdbff0b8acab0c7becfc518f416da29bf5
Beda Schmid <beda@tukutoi.com>
2023-10-14 20:41:10 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Require the GitHub_Theme_Updater class.
+ *
+ * Your theme will require GitHub_Theme_Updater to be loaded under a custom namespace.
+ * Ideally use an autoloader, but for the sake of functionality we are using require_once here.
+ */
+require_once get_template_directory() . 'update.php';
+use MyTheme\GitHub_Theme_Updater;
+( new GitHub_Theme_Updater( plugin_basename( __FILE__ ), '1.0.0', 'https://api.github.com/repos/smileBeda/github-wp-update' ) )->init();
